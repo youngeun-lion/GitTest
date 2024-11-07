ae546c83cdad004825f6e2a9d2a1a9f4a00682e4
user <ygwon7590@gmail.com>
2024-11-07 11:21:19 +0900

    Init Commit

--- app/src/main/java/com/lion/gittest/MainActivity.kt
+++ app/src/main/java/com/lion/gittest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.lion.gittest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +22,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Log.d("test100", "Aaaaaaa")
     }
 }
